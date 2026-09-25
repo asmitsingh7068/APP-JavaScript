b1f1e3a8ae015548593f8ff6beef9767f5f249be
asmitsingh7068 <asmitsingh20000@gmail.com>
2026-09-25 06:38:31 +0530

    string all function and task .

---
+++ string_array_task2/index.js
@@ -0,0 +1,356 @@
+// Task 01: Use startsWith() to check whether "JavaScript" starts with "Java".
+
+// let a = "JavaScript";
+// let x = a.startsWith("Java");
+// console.log(x);
+
+
+// Task 02: Use startsWith() to check whether "Hello World" starts with "Hello".
+
+// let b = "Hello World";
+// let y = b.startsWith("Hello");
+// console.log(y);
+
+
+// Task 03: Use startsWith() to check whether "Welcome to JavaScript" starts with "Welcome".
+
+// let c = "Welcome to JavaScript";
+// let z = c.startsWith("Welcome");
+// console.log(z);
+
+// Task 04: Use endsWith() to check whether "JavaScript" ends with "Script".
+
+// let a = "JavaScript";
+// let x = a.endsWith("Script");
+// console.log(x);
+
+
+// Task 05: Use endsWith() to check whether "Hello World" ends with "World".
+
+// let b = "Hello World";
+// let y = b.endsWith("World");
+// console.log(y);
+
+
+// Task 06: Use endsWith() to check whether "Welcome to JavaScript" ends with "JavaScript".
+
+// let c = "Welcome to JavaScript";
+// let z = c.endsWith("JavaScript");
+// console.log(z);
+
+// Task 07: Use search() to find the position of "JavaScript" in "I love JavaScript".
+
+// let a = "I love JavaScript";
+// let x = a.search("JavaScript");
+// console.log(x);
+
+
+// Task 08: Use search() to find the position of "World" in "Hello World".
+
+// let b = "Hello World";
+// let y = b.search("World");
+// console.log(y);
+
+
+// Task 09: Use search() to find the position of "easy" in "JavaScript is easy to learn".
+
+// let c = "JavaScript is easy to learn";
+// let z = c.search("easy");
+// console.log(z);
+
+// Task 10: Use indexOf() to find the position of "JavaScript" in "I love JavaScript".
+
+// let a = "I love JavaScript";
+// let x = a.indexOf("JavaScript");
+// console.log(x);
+
+
+// Task 11: Use indexOf() to find the first position of "Hello" in "Hello World Hello".
+
+// let b = "Hello World Hello";
+// let y = b.indexOf("Hello");
+// console.log(y);
+
+
+// Task 12: Use indexOf() to find the position of "powerful" in "JavaScript is powerful".
+
+// let c = "JavaScript is powerful";
+// let z = c.indexOf("powerful");
+// console.log(z);
+
+// Task 13: Use lastIndexOf() to find the last position of "Hello" in "Hello World Hello".
+
+// let a = "Hello World Hello";
+// let x = a.lastIndexOf("Hello");
+// console.log(x);
+
+
+// Task 14: Use lastIndexOf() to find the last position of "JavaScript" in "JavaScript JavaScript".
+
+// let b = "JavaScript JavaScript";
+// let y = b.lastIndexOf("JavaScript");
+// console.log(y);
+
+
+// Task 15: Use lastIndexOf() to find the last position of "apple" in "apple banana apple mango apple".
+
+// let c = "apple banana apple mango apple";
+// let z = c.lastIndexOf("apple");
+// console.log(z);
+
+// Task 16: Use charAt() to extract the first character of "JavaScript".
+
+// let a = "JavaScript";
+// let x = a.charAt(0);
+// console.log(x);
+
+
+// Task 17: Use charAt() to extract the 6th character of "Hello World".
+
+// let b = "Hello World";
+// let y = b.charAt(5);
+// console.log(y);
+
+
+// Task 18: Use charAt() to extract the last character of "JavaScript".
+
+// let c = "JavaScript";
+// let z = c.charAt(c.length - 1);
+// console.log(z);
+
+// Task 19: Use charCodeAt() to find the character code of "A".
+
+// let a = "A";
+// let x = a.charCodeAt(0);
+// console.log(x);
+
+
+// Task 20: Use charCodeAt() to find the character code of the first character of "JavaScript".
+
+// let b = "JavaScript";
+// let y = b.charCodeAt(0);
+// console.log(y);
+
+
+// Task 21: Use charCodeAt() to find the character code of the 3rd character of "Hello".
+
+// let c = "Hello";
+// let z = c.charCodeAt(2);
+// console.log(z);
+
+// Task 22: Use fromCharCode() to get the character from character code 65.
+
+// let a = String.fromCharCode(65);
+// let x = a;
+// console.log(x);
+
+
+// Task 23: Use fromCharCode() to create characters from 65, 66, and 67.
+
+// let b = String.fromCharCode(65, 66, 67);
+// let y = b;
+// console.log(y);
+
+
+// Task 24: Use fromCharCode() to create a word from 72, 101, 108, 108, and 111.
+
+// let c = String.fromCharCode(72, 101, 108, 108, 111);
+// let z = c;
+// console.log(z);
+
+// Task 25: Use concat() to join "Hello" and "World".
+
+// let a = "Hello";
+// let x = a.concat("World");
+// console.log(x);
+
+
+// Task 26: Use concat() to join "JavaScript" and "is Easy" to create a sentence.
+
+// let b = "JavaScript";
+// let y = b.concat(" is Easy");
+// console.log(y);
+
+// Task 28: Use repeat() to repeat "Hello " 3 times.
+
+// let a = "Hello ";
+// let x = a.repeat(3);
+// console.log(x);
+
+
+// Task 29: Use repeat() to repeat "JavaScript " 5 times.
+
+// let b = "JavaScript ";
+// let y = b.repeat(5);
+// console.log(y);
+
+
+// Task 30: Use repeat() to repeat "*" 10 times to create a line.
+
+// let c = "*";
+// let z = c.repeat(10);
+// console.log(z);
+
+
+// Task 31: Use substr() to extract the first 4 characters from "JavaScript".
+
+// let a = "JavaScript";
+// let x = a.substr(0, 4);
+// console.log(x);
+
+
+// Task 32: Use substr() to extract "World" from "Hello World".
+
+// let b = "Hello World";
+// let y = b.substr(6, 5);
+// console.log(y);
+
+
+// Task 33: Use substr() to extract "Programming" from "JavaScript Programming".
+
+// let c = "JavaScript Programming";
+// let z = c.substr(11);
+// console.log(z);
+
+// Task 34: Use substring() to extract "Java" from "JavaScript".
+
+// let a = "JavaScript";
+// let x = a.substring(0, 4);
+// console.log(x);
+
+
+// Task 35: Use substring() to extract "World" from "Hello World".
+
+// let b = "Hello World";
+// let y = b.substring(6, 11);
+// console.log(y);
+
+
+// Task 36: Use substring() to extract "Programming" from "JavaScript Programming".
+
+// let c = "JavaScript Programming";
+// let z = c.substring(12);
+// console.log(z);
+
+// Task 37: Use toString() to convert the number 100 into a string.
+
+// let a = 100;
+// let x = a.toString();
+// console.log(x);
+
+
+// Task 38: Use toString() to convert the number 500 into a string and find its length.
+
+// let b = 500;
+// let y = b.toString().length;
+// console.log(y);
+
+
+// Task 39: Use toString() to convert the array [10, 20, 30] into a string.
+
+// let c = [10, 20, 30];
+// let z = c.toString();
+// console.log(z);
+
+
+// Task 40: Use valueOf() to get the primitive string value of "JavaScript".
+
+// let a = "JavaScript";
+// let x = a.valueOf();
+// console.log(x);
+
+
+// Task 41: Use valueOf() to print the actual string value of "Hello World".
+
+// let b = "Hello World";
+// let y = b.valueOf();
+// console.log(y);
+
+
+// Task 42: Use valueOf() to get the value of a string and store it in another variable.
+
+// let c = "JavaScript";
+// let z = c.valueOf();
+// console.log(z);
+
+// Task 43: Use trimStart() to remove the starting spaces from " JavaScript".
+
+// let a = " JavaScript";
+// let x = a.trimStart();
+// console.log(x);
+
+
+// Task 44: Use trimStart() to print " Hello World" without starting spaces.
+
+// let b = " Hello World";
+// let y = b.trimStart();
+// console.log(y);
+
+
+// Task 45: Use trimStart() to remove only the starting spaces from " JavaScript " and keep the ending spaces.
+
+// let c = " JavaScript ";
+// let z = c.trimStart();
+// console.log(z);
+
+
+// Task 46: Use trimEnd() to remove the ending spaces from "JavaScript ".
+
+// let a = "JavaScript ";
+// let x = a.trimEnd();
+// console.log(x);
+
+
+// Task 47: Use trimEnd() to print "Hello World " without ending spaces.
+
+// let b = "Hello World ";
+// let y = b.trimEnd();
+// console.log(y);
+
+
+// Task 48: Use trimEnd() to remove only the ending spaces from " JavaScript " and keep the starting spaces.
+
+// let c = " JavaScript ";
+// let z = c.trimEnd();
+// console.log(z);
+
+// Task 49: Use replaceAll() to replace all "JavaScript" with "Python" in "JavaScript is easy. JavaScript is powerful."
+
+// let a = "JavaScript is easy. JavaScript is powerful.";
+// let x = a.replaceAll("JavaScript", "Python");
+// console.log(x);
+
+
+// Task 50: Use replaceAll() to replace all "cat" with "lion" in "cat dog cat bird cat".
+
+// let b = "cat dog cat bird cat";
+// let y = b.replaceAll("cat", "lion");
+// console.log(y);
+
+
+// Task 51: Use replaceAll() to replace all "HTML" with "Bootstrap" in "HTML CSS HTML JavaScript HTML".
+
+// let c = "HTML CSS HTML JavaScript HTML";
+// let z = c.replaceAll("HTML", "Bootstrap");
+// console.log(z);
+
+
+// Task 52: Use trim() and toUpperCase() to convert " javascript " into "JAVASCRIPT".
+
+// let a = " javascript ";
+// let x = a.trim().toUpperCase();// white space kato aur upperCase me change kro
+// console.log(x);
+
+
+// Task 53: Use startsWith(), endsWith(), and includes() with the string "JavaScript is a powerful language" to check the following conditions:
+// 1. Check whether the string starts with "JavaScript".
+// 2. Check whether the string ends with "language".
+// 3. Check whether the string contains "powerful".
+
+// let b = "JavaScript is a powerful language";
+// let y = b.startsWith("JavaScript");
+// let z = b.endsWith("language");
+// let p = b.includes("powerful");
+// console.log(y);
+// console.log(z);
+// console.log(p);
